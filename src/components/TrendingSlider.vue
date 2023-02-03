acfc4de75a7e1f1f1ebb64fed08b378221045585
asimovSO <svoleg2002@gmail.com>
2023-02-04 01:08:27 +0200

    installed swiper and made trending-slider component

---
+++ src/components/TrendingSlider.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from "vue";
+import TrendingSliderItem from "./TrendingSliderItem.vue";
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, EffectFade, Navigation } from 'swiper'
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
+import 'swiper/css/navigation';
+
+const data = ref([
+  {
+    id: 1,
+    title: 'MEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGANMEGAN',
+    poster: 'https://image.tmdb.org/t/p/original/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg',
+    overview: "A brilliant toy company roboticist uses artificial intelligence to develop M3GAN, a life-like doll programmed to emotionally bond with her newly orphaned niece. But when the doll's programming works too well, she becomes overprotective of her new friend with terrifying results."
+  },
+  {
+    id: 2,
+    title: 'MEGAN',
+    poster: 'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/11/avatar-the-way-of-water-poster.jpg',
+    overview: "A brilliant toy company roboticist uses artificial intelligence to develop M3GAN, a life-like doll programmed to emotionally bond with her newly orphaned niece. But when the doll's programming works too well, she becomes overprotective of her new friend with terrifying results."
+  },
+  {
+    id: 3,
+    title: 'MEGAN',
+    poster: 'https://www.vibe.com/wp-content/uploads/2022/10/BlackPanther-WakandaForever_Payoff_1-Sht_v10_lg-e1665517283161.jpg?w=1024',
+    overview: "A brilliant toy company roboticist uses artificial intelligence to develop M3GAN, a life-like doll programmed to emotionally bond with her newly orphaned niece. But when the doll's programming works too well, she becomes overprotective of her new friend with terrifying results."
+  }
+])
+
+
+const autoplayOptions = {
+  delay: 3500,
+  pauseOnMouseEnter: true,
+  disableOnInteraction: true,
+}
+
+// const modules = [Autoplay, EffectFade, Navigation]
+
+</script>
+
+<template>
+
+    <Swiper 
+      :modules="[EffectFade, Autoplay]" 
+      class="h-[400px] w-full"
+      :autoplay="autoplayOptions"
+      :grabCursor="true"
+      effect='fade' 
+      :loop="true"
+      :spaceBetween="1"
+      >
+        <SwiperSlide v-for="(trend, index) in data" :key="trend.id" >
+          <TrendingSliderItem :id="trend.id" :title="trend.title" :overview="trend.overview" :poster="trend.poster" />
+        </SwiperSlide>
+    </Swiper>
+  
+</template>
+
+<style scoped>
+h1 {
+  text-overflow: ellipsis;
+  overflow-x: hidden;
+}
+
+.desc {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -moz-box;
+  -moz-box-orient: vertical;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  line-clamp: 4;
+  box-orient: vertical;
+}
+</style>
---
+++ src/components/TrendingSliderItem.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ChevronRightIcon, StarIcon } from "@heroicons/vue/24/solid";
+
+defineProps({
+  id: [String, Number],
+  title: String,
+  poster: String,
+  overview: String
+})
+</script>
+
+<template>
+  <div class="h-full w-[890px] relative left-1/2 top-0 -translate-x-1/2">
+    <img :src="poster" alt="" class="w-full h-full object-cover bg-center rounded ">
+    <div class="rounded py-6 px-4 absolute bg-black/40 backdrop-blur-[2px] top-1/2 left-8 translate-y-[-50%] h-64 max-w-[50%]
+      flex flex-col justify-between gap-4">
+      <div>
+        <h1 class="text-2xl mb-4 font-bold">{{ title }}</h1>
+        <p class="desc text-white/90 text-base">{{ overview }}</p>
+      </div>
+      <div class="flex justify-between items-center gap-2">
+        <button class="rounded bg-rose-600 px-4 py-2 w-fit font-medium">Watch</button>
+        <div class="flex items-center gap-1">
+          <StarIcon class="w-6 text-yellow-400" />
+          <span class="text-xl font-medium">7.2</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-overflow: ellipsis;
+  overflow-x: hidden;
+}
+
+.desc {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -moz-box;
+  -moz-box-orient: vertical;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  line-clamp: 4;
+  box-orient: vertical;
+}
+</style>
