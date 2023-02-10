008acf00f79c7d0a9540316c9a6e7de4d93e31d7
asimovSO <svoleg2002@gmail.com>
2023-02-10 15:58:56 +0200

    fixed FavforitePage and FavoriteCard

--- src/components/FavoriteCard.vue
+++ src/components/FavoriteCard.vue
@@ -1,20 +1,21 @@
 <template>
   <div
-    class="w-full h-[200px]  border-2 border-rose-600 rounded-md mr-4 flex justify-between"
+    class="w-full h-[200px] border-2 border-rose-600/50 rounded-md gap-4 flex justify-between"
   >
-    <router-link
-      :to="{ name: 'moviePage', params: {  id: id } }"
-    >
+    <router-link :to="{ name: 'moviePage', params: { id: id } }">
       <div class="flex gap-4 h-full">
         <img
           :src="'https://image.tmdb.org/t/p/w500/' + poster"
           alt=""
-          class="block h-full object-cover max-w-[250px] -z-10"
+          class="block h-full object-cover -z-10"
         />
-        <div class="title text-xl font-medium py-2">{{ title }}</div>
+        <div class="title text-xl font-medium py-2 w-fit">{{ title }}</div>
       </div>
     </router-link>
-    <button @click.stop="store.deleteFavoriteMovie(id)" class="p-4 self-center">
+    <button
+      @click.stop="store.deleteFavoriteMovie(id)"
+      class="block p-4 self-center"
+    >
       <XMarkIcon class="text-rose-600 w-6" />
     </button>
   </div>
--- src/pages/FavoritePage.vue
+++ src/pages/FavoritePage.vue
@@ -9,7 +9,7 @@ const favMovies = ref()
 </script>
 
 <template>
-  <div class="grid grid-cols-2 gap-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     <FavoriteCard
       v-for="fav in store.favoriteMoviesData"
       :key="fav.id"
